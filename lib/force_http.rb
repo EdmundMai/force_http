d007c1c01cc4f63c02abaf0a0307b1ba1309fd4e
Edmund <edmundmai@gmail.com>
2013-09-05 15:52:15 -0400

    fixed syntax errors

--- lib/force_http.rb
+++ lib/force_http.rb
@@ -11,9 +11,9 @@ module ForceHttp
       host = options.delete(:host)
       before_filter(options) do
         if request.ssl? && !Rails.env.development?
-          redirect_options = {:protocol => 'http://', :status => :moved permanently}
+          redirect_options = {:protocol => 'http://', :status => :moved_permanently}
           redirect_options.merge!(:host => host) if host
-          redirect_options.merge!(:params => request.query parameters)
+          redirect_options.merge!(:params => request.query_parameters)
           redirect_to redirect_options
         end
       end
@@ -22,6 +22,6 @@ module ForceHttp
   
 end
 
-class ActionController
+class ActionController::Base
   include ForceHttp
 end
